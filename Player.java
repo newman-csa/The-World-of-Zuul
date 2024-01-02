2f0033091b0401cfdf32a217f84d8fe99c271dd5
newman-csa <146094244+newman-csa@users.noreply.github.com>
2024-01-02 09:27:12 -0500

    Create Player.java and Update Item

---
+++ Player.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Item> invetory;
+
+    public Player() {}
+
+    public void collectItem()
+
+    public void useItem(String name) {
+        
+    }
+}
